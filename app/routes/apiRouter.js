98e1925cb22800bb3ae892f29c70d87e176dea88
Tira28 <YWachyar@nl.ibm.com>
2016-07-25 00:07:51 +0200

    modifying apiRouter.js

--- app/routes/apiRouter.js
+++ app/routes/apiRouter.js
@@ -2,10 +2,6 @@ var express = require('express');
 var api = express.Router();
 var jsonQuery = require('json-query');
 
-var ALLOWED_IPS = [
-  "127.0.0.1",
-  "123.456.7.89"
-];
 
 var usersData = [{
     name: 'Tira',
@@ -17,16 +13,8 @@ var usersData = [{
     job: 'lawyer',
     office: 'utrecht'
   }
-]
+];
 
-api.use(function(req,res,next){
-  var userIsAllowed = ALLOWED_IPS.indexOf(req.ip) !== -1;
-  if (!userIsAllowed){
-    res.status(401).send("Not Authorized");
-  } else {
-    next();
-  }
-});
 
 api.get('/users', function(req,res){
   res.json(usersData);
@@ -34,10 +22,13 @@ api.get('/users', function(req,res){
 
 api.get('/users/:name', function(req,res){
   var userName = String(req.params.name);
-  console.log(usersData.find(function(data){
-    data.name === userName;
-  }));
-  res.send();
+  var userIndex = usersData.findIndex(user=>user.name==userName);
+  if(usersData[userIndex]){
+    res.json(usersData[userIndex]);
+  } else {
+    res.send('user is not found');
+  }
 });
+
 
 module.exports = api;
